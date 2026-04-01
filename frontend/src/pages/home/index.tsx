1401b17019ff84dd3bacde5a3e0ed6e528e8a5b2
lcc <948200163@qq.com>
2026-04-01 19:37:58 +0800

    feat: '修改模板'

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,15 +1,13 @@
 import './index.less';
 import reactImg from '@/assets/react.svg';
-
-const routerBasename =
-  __APP_ROUTER_BASENAME__ === '/' ? '' : __APP_ROUTER_BASENAME__.replace(/\/$/, '');
+import { getPublicPath } from '@/tools/utils';
 
 const Home = () => {
   return (
     <div>
       <img width={200} height={200} src={reactImg} alt="" />
-      <img width={200} height={200} src={`${routerBasename}/public/vite.svg`} alt="" />
-      <img width={200} height={200} src={`${routerBasename}/public/Turret10.png`} alt="" />
+      <img width={200} height={200} src={getPublicPath('vite.svg')} alt="" />
+      <img width={200} height={200} src={getPublicPath('Turret10.png')} alt="" />
       home
     </div>
   );
---
+++ frontend/src/tools/utils.ts
@@ -0,0 +1,7 @@
+// Get public path
+export function getPublicPath(path: string): string {
+  const baseRouter =
+    __APP_ROUTER_BASENAME__ === '/' ? '' : __APP_ROUTER_BASENAME__.replace(/\/$/, '');
+  const normalizedPath = path.replace(/^\/+/, '');
+  return `${baseRouter}/${normalizedPath}`;
+}
